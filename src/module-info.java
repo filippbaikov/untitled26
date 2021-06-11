1bc1427e4a67cd7ecff9e5f2d8d7b5c7b95509d7
filipp <zeliboba0@bk.ru>
2021-06-11 23:01:07 +0300

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module untitled26 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+    opens sample;
+
+}
---
+++ src/sample/Controller.java
@@ -0,0 +1,21 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    TextArea MyTextArea;
+    @FXML
+    TextField MyTextField;
+
+
+    public void Send(ActionEvent actionEvent) {
+        MyTextArea.appendText(MyTextField.getText()+"\n");
+        MyTextField.clear();
+
+
+    }
+}
